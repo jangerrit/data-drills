e01ebfc2d2ea433ec79217e8f78a0adc401af783
Jan Gerrit Post <post@jangerrit.nl>
2026-05-07 15:35:22 +0200

    Add Booking Breakdown

---
+++ Booking Breakdown/Booking Breakdown.R
@@ -0,0 +1,35 @@
+# Libraries
+library("tidyverse")
+library("formattable")
+
+# Data
+bookings <- read_csv("~/Documents/GitHub/data-drills/Booking Breakdown/hotel_bookings.csv", col_types = "iDDDDl")
+capacity <- 200
+
+# Objective
+occupancy <- bookings |>
+  filter_out(is_canceled) |>
+  right_join(
+    tibble(
+      date = seq(
+        min(bookings$checkin_date),
+        max(bookings$checkout_date)
+      )
+    ),
+    join_by(
+      checkin_date <= date,
+      checkout_date > date
+    )
+  ) |>
+  group_by(
+    Year = year(date),
+    Month = month(date, label = TRUE, abbr = TRUE)
+  ) |>
+  summarise(
+    Occupations = n(),
+    Capacity = n_distinct(date) * capacity,
+    Occupancy = percent(Occupations / Capacity, digits = 1),
+  )
+
+# Check
+filter(occupancy, Year == 2016, Month == "Jul")
---
+++ Booking Breakdown/index.md
@@ -0,0 +1,21 @@
+# Booking Breakdown
+
+[Source](https://mavenanalytics.io/data-drills/booking-breakdown)
+
+# Objective
+
+Your dataset contains ~40,000 reservations for a 200-room resort hotel in Portugal, including the booking date, check-in & check-out dates, and a cancellation flag.
+
+Your task is to calculate the resort's occupancy rate for each month in the dataset.
+
+- Occupancy rate = booked room-nights ÷ available room-nights
+- A guest occupies a room every night from their check-in date up to (but not including) their check-out date
+- Canceled reservations should not count toward occupancy
+
+# Control question
+
+What was the resort's occupancy rate in July 2016? (digits only, rounded down to the nearest percentage point))
+
+# Correct answer
+
+88
