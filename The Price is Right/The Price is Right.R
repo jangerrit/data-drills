64c7829491f726fd5c61dbb85ea3819563d4cdea
Jan Gerrit Post <post@jangerrit.nl>
2026-05-07 17:21:58 +0200

    Simplify code

--- The Price is Right/The Price is Right.R
+++ The Price is Right/The Price is Right.R
@@ -7,18 +7,17 @@ transactions <- read_csv(str_c(path, "transactions.csv"))
 price_history <- read_csv(str_c(path, "price_history.csv"))
 
 # Objective
-prices <- transactions |>
-  left_join(price_history, join_by(
-    pizza_id, closest(order_date >= effective_date))
+prices <- left_join(
+  transactions,
+  price_history,
+  join_by(
+    pizza_id,
+    closest(order_date >= effective_date)
   )
+)
 
 # Check
-prices |>
-  mutate(revenue = price * quantity) |>
-  summarise(
-    orders = n_distinct(order_id),
-    transactions = n(),
-    price = round(mean(price), digits = 2),
-    quantity = sum(quantity),
-    revenue = round(sum(revenue), digits = 0)
-  )
+summarise(
+  prices,
+  revenue = round(sum(price * quantity))
+)
